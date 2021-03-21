8be678074d33a91422f88a6164d304c49e27f653
Nigel <nigelswab@uvic.ca>
2021-03-21 15:32:01 -0700

    Test Live reading and change of vars

--- Sandbox/Pi/pi-ard-2way.py
+++ Sandbox/Pi/pi-ard-2way.py
@@ -13,18 +13,26 @@ arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 # Get rid of garbage/incomplete data
 arduino.flush()
 
+#open file
+f = open("params.txt", "r")
+print(f.readline())
+params = f.readline()
+
 # Infinite loop
 while (1):
-    send_string = ("50,10,3 \n")
+    if params != f.readline():
+        params = f.readline()
 
-    # Send the string. Make sure you encode it before you send it to the Arduino.
-    arduino.write(send_string.encode('utf-8'))
-
-    # Do nothing for 500 milliseconds (0.5 seconds)
-    time.sleep(0.5)
-
+        # Send the string. Make sure you encode it before you send it to the Arduino.
+        # arduino.write(send_string.encode('utf-8'))
+        print(params)
+        # Do nothing for 500 milliseconds (0.5 seconds)
+        time.sleep(0.5)
+    else:
     # Receive data from the Arduino
-    receive_string = arduino.readline().decode('utf-8').rstrip()
+    #receive_string = arduino.readline().decode('utf-8').rstrip()
 
     # Print the data received from Arduino to the terminal
-    print(receive_string)
+    #print(receive_string)
+    print("nothing new")
+    time.sleep(1)
