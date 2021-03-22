d7fda61fedf0a33340601e4dd765b28fb4789235
Nigel <nigelswab@uvic.ca>
2021-03-21 21:30:45 -0700

    Updating logger to add settings to metadata

--- Sandbox/Pi/csv_logger.py
+++ Sandbox/Pi/csv_logger.py
@@ -6,12 +6,12 @@ class Logger:
     def __init__(self, fileName: str,
                  test_plan: str,
                  test_notes: str,
-                 firmware: str):
+                 settings: dict[str, str]):
         
         self.fileName = self.remove_bad_chars(fileName)
         self.test_plan = test_plan
         self.test_notes = test_notes
-        self.firmware = firmware
+        self.settings = settings
         
         # set folder and file path
         self.folderPath ='/home/pi/BlueFish/data/' + datetime.today().strftime("%Y-%m-%d") 
@@ -26,27 +26,30 @@ class Logger:
         # Insert metadata
         standard_meta = {'start_timestamp': datetime.today().strftime('%Y-%m-%d - %H:%M:%S'),
                          'test_plan': self.test_plan,
-                         'firmware': self.firmware,
-                         'test_notes': self.test_notes or ''
+                         'test_notes': self.test_notes or '',
+                         'settings': self.settings,
                          }
         
         # insert meta into the top of the csv
         for meta in standard_meta:
             #append the data to the file
             self.file = open(self.filePath, "a")
-            #write data with a new line
-            self.file.write(meta + "," + standard_meta[meta] + '\n')
-        self.file.close()
 
+            if meta == 'settings':
+                for key in self.settings:
+                    self.file.write(key + ',' + self.settings[key])
+            else:
+                #write data with a new line
+                self.file.write(meta + "," + standard_meta[meta] + '\n')
+        self.file.close()
 
     def log_row(self, data: str):
-        #append the data to the file
+        # append the data to the file
         self.file = open(self.filePath, "a")
-        #write data with a new line
+        # write data with a new line
         self.file.write(data + "\n")
-        #close file
+        # close file
         self.file.close()
-                
 
     @staticmethod
     def remove_bad_chars(string: str) -> str:
