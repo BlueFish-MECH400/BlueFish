2979f4e888b74a4e613a518d04fa0d958da37fec
Nigel <nigelswab@uvic.ca>
2021-03-21 17:50:37 -0700

    Adding readability functionality

--- Sandbox/Pi/pi-ard-2way.py
+++ Sandbox/Pi/pi-ard-2way.py
@@ -5,7 +5,7 @@ Author: Nigel Swab
 March 14, 2021
 '''
 
-import serial
+# import serial
 import time
 
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -13,23 +13,23 @@ arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 # Get rid of garbage/incomplete data
 arduino.flush()
 
-#open file
+# open file
 F = open("params.txt", "r")
-print(F.readline())
 params = F.read()
 F.close()
 
 # Infinite loop
-while (1):
+while 1:
     f = open("params.txt", "r")
     comp = f.read()
-    if params != comp:
-        params = comp
 
+    # Check if parameter file has changed
+    if params != comp:
+
+        params = comp
         # Send the string. Make sure you encode it before you send it to the Arduino.
-        # arduino.write(send_string.encode('utf-8'))
-        print(params)
-        print("^")
+        arduino.write(send_string.encode('utf-8'))
+        print("Parameters update: " + params)
         # Do nothing for 500 milliseconds (0.5 seconds)
         time.sleep(1)
         f.close()
