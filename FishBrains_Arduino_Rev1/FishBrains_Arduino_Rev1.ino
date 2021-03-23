d8d50645a61e6d34e941d62ed47c21117fc874dd
Clayton <ctmoxley@gmail.com>
2021-03-23 16:13:49 -0600

    Updated Arduino code with ISRs, interrupt, calibration LEDs etc

--- FishBrains_Arduino_Rev1/FishBrains_Arduino_Rev1.ino
+++ FishBrains_Arduino_Rev1/FishBrains_Arduino_Rev1.ino
@@ -92,6 +92,15 @@ void outputPID(void);   // Display PID I/O (for debugging)
 
 void setup(void)
 {
+  /* Set I/O pinmodes */
+  pinMode(LEAK_PIN, INPUT);  // Set leak sensor pin to INPUT
+  pinMode(EINT1_PIN, INPUT); // Set interrupt pin to INPUT
+  pinMode(LED_1_PIN, OUTPUT);  // Set LED1 pin to OUTPUT
+  pinMode(LED_2_PIN, OUTPUT);  // Set LED2 pin to OUTPUT
+  pinMode(LED_3_PIN, OUTPUT);  // Set LED3 pin to OUTPUT
+
+  bno.setExtCrystalUse(true); // Used for BNO055
+
   /* Initialize serial and I2C */
   Serial.begin(BAUD_RATE);  // Initialize serial at baud rate
   pingSerial.begin(BAUD_RATE);  // Initialize software serial at baud rate
@@ -99,34 +108,24 @@ void setup(void)
 
   initSensor(); // Checks if sensors are functioning
 
-  /* Set I/O pinmodes */
-  pinMode(LED_PIN, OUTPUT);  // Set LED pin to OUTPUT
-  pinMode(LEAK_PIN, INPUT);  // Set leak sensor pin to INPUT
-  pinMode(EINT1_PIN, INPUT); // Set interrupt pin to INPUT
-
+  attachInterrupt(digitalPinToInterrupt(EINT1_PIN),updateSettings,RISING);
 
    /* Attach servos and write initial position */
   servo1.attach(SERVO_1_PIN);  // Attach servo1 to servo1 pin
   servo2.attach(SERVO_2_PIN); // Attach servo2 to servo2 pin
   servo1.write(INIT_SERVO_POS); // Set servo1 position to initial position
   servo2.write(INIT_SERVO_POS); // Set servo2 position to initial position
-
+   
   /* Turn on PID and set output limits */
   pitchPID.SetMode(AUTOMATIC); // Set pitch PID mode automatic (ON)
   rollPID.SetMode(AUTOMATIC); // Set roll PID mode automatic (ON)
-  pitchPID.SetOutputLimits(-90,90); // Set pitch PID limits to servo limits
-  rollPID.SetOutputLimits(-90,90); // Set roll PID limits to servo limits
-
-
-  bno.setExtCrystalUse(true); // Used for BNO055
+  pitchPID.SetOutputLimits(-SERVO_LIMIT,SERVO_LIMIT); // Set pitch PID limits to servo limits
+  rollPID.SetOutputLimits(-SERVO_LIMIT,SERVO_LIMIT); // Set roll PID limits to servo limits
 
   /* Bar 30 sensor setup */
   bar30.setModel(MS5837::MS5837_30BA);
   bar30.setFluidDensity(SEA_WATER); // Set fluid density to sea water
-
-  /* Print sensor data headers to serial */
-  Serial.println("Altitude,Depth,Pressure,Temperature,X,Y,Z");
-
+  
   delay(2000); // Delay for IMU calibration
 }
 
@@ -134,102 +133,108 @@ void setup(void)
 /*      Main Routine                                                      */
 /**************************************************************************/
 
-void loop(void)
-{
-  state = IDLE;
+void loop(void){
+  
+  displayCalStatus(); // Wait until BNO055 calibrated (display status on LEDs)
+
+  /* Print sensor data headers to serial */
+  //Serial.println("Altitude,Depth,Pressure,Temperature,X,Y,Z"); REMOVE IF NOT NEEDED
   goto RUN_BLUEFISH;
+  
 
   RUN_BLUEFISH:
 
-    switch(state){
+    currentTime = millis(); // Set current time
+    /* Check for leak */
+    leak = digitalRead(LEAK_PIN);// Read the Leak Sensor Pin
+
+    if (leak == HIGH) { // If leak detected
+      state = DEPTH;  // Set state to DEPTH mode
+      leakWarningFlash(); // Flash LEDs indicating leak
+      goto MODE_SWITCH;
+    }
+    
+    logPeriod = (1/logRate)*1000; // Convert log rate in Hz to period in milliseconds
+
+    if((currentTime-transmitTime)>=logPeriod) {  // Check if time to transmit data
+      readSensors();
+      transmitData(); // Transmit data to Raspberry Pi
+    }
+    
+    goto MODE_SWITCH;
+
+  MODE_SWITCH:
+
+    switch(state) {
 
       case IDLE:
-        goto IDLE_STATE;
+        goto IDLE_MODE;
         break;
 
-      case STABILIZE:
-        goto STABILIZE_STATE;
+      case DEPTH:
+        goto DEPTH_MODE;
         break;
 
-      case DIVE:
-        goto DIVE_STATE;
+      case ALTITUDE:
+        goto ALTITUDE_MODE;
         break;
 
       case SURFACE:
-        goto SURFACE_STATE;
+        goto SURFACE_MODE;
         break;
 
       default:
         goto RUN_BLUEFISH;
     }
 
-    IDLE_STATE:
-
+    IDLE_MODE:
+      
+      servo1.write(INIT_SERVO_POS);
+      servo2.write(INIT_SERVO_POS);
       goto RUN_BLUEFISH;
 
-    STABILIZE_STATE:
+    DEPTH_MODE:
+      
+      if (leak == HIGH) { // If leak detected
+        targetDepth = 0;  // Set target depth to 0 (surface)
+      }
+
+      readSensors();  // Read sensor data
+      pitchSetpoint = targetDepth; // Set pitch setpoint to target depth
+      pitchInput = depth; // Set PID pitch input to depth
+      runPID(); // Run PID to computed servo outputs
+      servo1.write(Out1); // Write output to servo1
+      servo2.write(Out2); // Write output to servo2
+      delay(mDelay);
+
       goto RUN_BLUEFISH;
+    
+    ALTITUDE_MODE:
+      
+      readSensors();
 
-    DIVE_STATE:
+      if(altitude <1) { // Check if close to seafloor
+        servo1.write(servoMin); // Set servo1 position to min
+        servo2.write(servoMax); // Set servo1 position to max
+        delay(mDelay);
+      }else {
+        pitchSetpoint = targetAltitude; // Set pitch setpoint to target altitude
+        pitchInput = altitude; // Set PID pitch input to altitude
+        runPID(); // Run PID to computed servo outputs
+        servo1.write(Out1); // Write output to servo1
+        servo2.write(Out2); // Write output to servo2
+        delay(mDelay);
+      }
+
       goto RUN_BLUEFISH;
+    
+    SURFACE_MODE:
+      
+      servo1.write(INIT_SERVO_POS); // Set servo positions to initial position
+      servo2.write(INIT_SERVO_POS); 
+      delay(mDelay);
 
-    SURFACE_STATE:
       goto RUN_BLUEFISH;
-
-  /* Check for leak */
-  leak = digitalRead(LEAK_PIN);   // Read the Leak Sensor Pin
-  digitalWrite(LED_PIN, leak);  // Sets the LED to the Leak Sensor's Value
-
-  if (leak == 1) {
-    Serial.println("Leak Detected!");
-  }
-
-  /* Get a new sensor event */
-  sensors_event_t event;
-  bno.getEvent(&event);
-
-  /* Read Ping */
-  if (ping.update()) {
-    altitude = ping.distance();
-  }
-
-/* If near surface, set foil angle to 45 degrees  */
-  if(altitude <5){
-    //set states
-    servo1.write(45);
-    servo2.write(135);
-    delay(15);
-  }
-  else{
-
-  /* Compute PID outputs for pitch and roll  */
-  pitchInput = event.orientation.y;  // Pitch input = y position
-  pitchPID.Compute();
-  OutP = pitchOutput;
-
-  rollInput = event.orientation.z;  // Roll input = z position
-  rollPID.Compute();
-  OutR = rollOutput;
-
-  /* Compute combined roll and pitch outputs */
-  Out1 = 90 - OutP + OutR;
-  Out2 = 90 + OutP + OutR;
-
-  /* Write combined output to servos*/
-  servo1.write(Out1);
-  servo2.write(Out2);
-  }
-
-
-/* Read Bar30 Sensor Data*/
-  bar30.read();
-  pressure = bar30.pressure();
-  temperature = bar30.temperature();
-  depth = bar30.depth();
-
-
-  /* Wait specified delay before requesting next data */
-  delay(BNO055_SAMPLERATE_DELAY_MS);
 }
 /*---- End of Main Routine -----------------------------------------------*/
 
@@ -238,47 +243,132 @@ void loop(void)
 /*      Function Definitions                                              */
 /**************************************************************************/
 
-
-/*------Check Initialization of Sensors-----------------------------------*/
 /*========================================================================*/
-void initSensor(void)
-{
-  /* Sensor initialization checks */
+/*------Sensor Intitialziation Verification-------------------------------*/
+/*========================================================================*/
+void initSensor(void) {
+  int bnoC, bar30C, pingC;  // Sensor status: 0 = connected, 1 = disconnected
 
-  if(!bno.begin())  //check if BNO055 functioning
-  {
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+  if(!bno.begin()){  //check if BNO055 functioning
+    bnoC = 1;
   }
-  if(!bar30.init())  //check if Bar30 functioning
-  {
-    Serial.println("Bar30 Init failed!");
-    Serial.println("Are SDA/SCL connected correctly?");
-    Serial.println("Blue Robotics Bar30: White=SDA, Green=SCL");
-    Serial.println("\n\n\n");
-    while(1);
+  if(!bar30.init()) {  //check if Bar30 functioning
+    bar30C  = 1;
   }
-
   if(!ping.initialize()) {  //check if Ping functioning
-      Serial.println("\nPing device failed to initialize!");
-      Serial.println("Are the Ping rx/tx wired correctly?");
-      Serial.print("Ping rx is the green wire, and should be connected to Arduino pin ");
-      Serial.print(SRX_PIN);
-      Serial.println(" (Arduino tx)");
-      Serial.print("Ping tx is the white wire, and should be connected to Arduino pin ");
-      Serial.print(STX_PIN);
-      Serial.println(" (Arduino rx)");
-    while(1);
+    pingC = 1;
+  }
+   /* Blink LED while sensor is not initiailized */
+   /* LED1 = BNO055, LED2 = Bar30, LED3 = Ping */
+  while(!bno.begin()||!bar30.init()||!ping.initialize()) {
+    if(bnoC == 1) {
+      digitalWrite(LED_1_PIN,HIGH);
+    }
+    if(bar30C == 1) {
+      digitalWrite(LED_1_PIN,HIGH);
+    }
+    if(pingC == 1) {
+      digitalWrite(LED_1_PIN,HIGH);
+    }
+    delay(sDelay);
+    digitalWrite(LED_1_PIN,LOW);
+    digitalWrite(LED_2_PIN,LOW);
+    digitalWrite(LED_3_PIN,LOW);
+  }
+}
+
+/*========================================================================*/
+/*------Display sensor calibration status (For Debugging)-----------------*/
+/*========================================================================*/
+void displayCalStatus(void) {
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  
+  /* While all values not calibrated, get calibration status.*/
+  /* 3 means 'fully calibrated" and requires system > 0  */
+  /* When fully calibrated, all LEDs are on. */
+  /* (LED1 = gyro, LED2 = accel, LED3 = mag) */
+  while((gyro!= 3) && (accel!= 3) && (mag!= 3) && (system<= 1)){
+    
+    /* Get the four calibration values (0..3) */
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+
+    if(gyro==3){
+      digitalWrite(LED_1_PIN,HIGH);
+    }else{
+      digitalWrite(LED_1_PIN,LOW);
+    }
+
+    if(accel==3){
+      digitalWrite(LED_1_PIN,HIGH);
+    }else{
+      digitalWrite(LED_1_PIN,LOW);
+    }
+
+    if(mag==3){
+      digitalWrite(LED_1_PIN,HIGH);
+    }else{
+      digitalWrite(LED_1_PIN,LOW);
+    }
   }
 
+  Serial.println("Calibration Complete");
+}
+
+/*========================================================================*/
+/*------Leak Sensor Warning-----------------------------------------------*/
+/*========================================================================*/
+void leakWarningFlash(void) {
+      if((currentTime-lastTime)>=sDelay) {
+        lastTime = currentTime;
+      
+        if(leakState==LOW) {
+          leakState = HIGH;
+          digitalWrite(LED_1_PIN, leakState);
+          digitalWrite(LED_2_PIN, leakState);
+          digitalWrite(LED_3_PIN, leakState);
+        }else if(leakState == HIGH) {
+          leakState = LOW;
+          digitalWrite(LED_1_PIN, leakState);
+          digitalWrite(LED_2_PIN, leakState);
+          digitalWrite(LED_3_PIN, leakState);
+        } 
+      } 
+}
+
+/*========================================================================*/
+/*------Read Sensor Data--------------------------------------------------*/
+/*========================================================================*/
+void readSensors(void) {
+  /* Read Bar30 sensor data */
+  bar30.read();
+  pressure = bar30.pressure();
+  temperature = bar30.temperature();
+  depth = bar30.depth();
+
+  /* Read BNO055 sensor data */
+
+   /* Get a new sensor event */
+  sensors_event_t event;
+  bno.getEvent(&event);
+  /* Store data */
+  dx = event.orientation.x;
+  dy = event.orientation.y;
+  dz = event.orientation.z;
+  gx = event.gyro.x;
+  gy = event.gyro.y;
+  gz = event.gyro.z;
+
+  /* Read Ping sensor Data*/
+  if (ping.update()) {
+    altitude = ping.distance();
+  }
 }
 
 /*========================================================================*/
 /*------Transmit Data via Serial------------------------------------------*/
 /*========================================================================*/
-
-void transmitData(void)
-{
+void transmitData(void) {
   /* Transmit sensor data */
   Serial.print(ping.distance());
   Serial.print(",");
@@ -288,48 +378,65 @@ void transmitData(void)
   Serial.print(",");
   Serial.print(bar30.temperature());
   Serial.print(",");
-  Serial.print(event.orientation.x, 4);
+  Serial.print(event.orientation.x, 2); // THESE WILL LIKELY CHANGE TO ERRORS
   Serial.print(",");
-  Serial.print(event.orientation.y, 4);
+  Serial.print(event.orientation.y, 2);
   Serial.print(",");
-  Serial.println(event.orientation.z, 4);
+  Serial.println(event.orientation.z, 2);
 }
 
 /*========================================================================*/
-/*------Display sensor calibration status (For Debugging)-----------------*/
+/*------Compute PID Output------------------------------------------------*/
 /*========================================================================*/
-void displayCalStatus(void)
-{
-  /* Get the four calibration values (0..3) */
-  /* Any sensor data reporting 0 should be ignored, */
-  /* 3 means 'fully calibrated" */
-  uint8_t system, gyro, accel, mag;
-  system = gyro = accel = mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
+void runPID(void) {
+  /* Compute PID outputs for pitch and roll  */
+  pitchPID.Compute();
+  OutP = pitchOutput;
 
-  /* The data should be ignored until the system calibration is > 0 */
-  Serial.print("\t");
-  if (!system)
-  {
-    Serial.print("! ");
+  rollInput = dz;  // Roll input = z position
+  rollPID.Compute();
+  OutR = rollOutput;
+
+  /* Compute combined roll and pitch outputs */
+  Out1 = 90 - OutP + OutR;
+  Out2 = 90 + OutP + OutR;
+}
+
+/*========================================================================*/
+/*------Interrupt ISR for Updating Settings-------------------------------*/
+/*========================================================================*/
+void updateSettings() {
+  if(Serial.available() > 0) {
+    String temp = Serial.readStringUntil(',');
+    logRate = temp.toDouble();
+    temp = Serial.readStringUntil(',');
+    targetDepth = temp.toDouble();
+    temp = Serial.readStringUntil(',');
+    targetAltitude = temp.toDouble();
+    temp = Serial.readStringUntil(',');
+    targetAltitude = temp.toDouble();
+    temp = Serial.readStringUntil(',');
+    state = temp.toInt();
+    temp = Serial.readStringUntil(',');
+    pKp = temp.toDouble();
+    temp = Serial.readStringUntil(',');
+    pKi = temp.toDouble();
+    temp = Serial.readStringUntil(',');
+    pKd = temp.toDouble();
+    temp = Serial.readStringUntil(',');
+    rKp = temp.toDouble();
+    temp = Serial.readStringUntil(',');
+    rKi = temp.toDouble();
+    temp = Serial.readStringUntil(',');
+    rKd = temp.toDouble();
   }
-
-  /* Display the individual values */
-  Serial.print("Sys:");
-  Serial.print(system, DEC);
-  Serial.print(" G:");
-  Serial.print(gyro, DEC);
-  Serial.print(" A:");
-  Serial.print(accel, DEC);
-  Serial.print(" M:");
-  Serial.print(mag, DEC);
 }
+
 /*========================================================================*/
 /*------Display PID I/O (For Debugging)-----------------------------------*/
 /*========================================================================*/
 
-void outputPID(void)
-{
+void outputPID(void){
   /* Display PID pinput and output values in serial*/
   Serial.print("Pitch PID Input: ");
   Serial.println(pitchInput);
