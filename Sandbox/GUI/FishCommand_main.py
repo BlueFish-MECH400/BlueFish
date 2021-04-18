a3058c3667946bed73e1cce2a9ad8b4c3b0b7f9e
nigelswab <nigelswab@uvic.ca>
2021-04-17 22:58:14 -0700

    minor bug fixes

--- Sandbox/GUI/FishCommand_main.py
+++ Sandbox/GUI/FishCommand_main.py
@@ -26,7 +26,7 @@ import serial
 import gpiozero
 
 INTERRUPT = gpiozero.LED(17)  # setup GPIO and ports for raspberry pi interrupt pin 11 (GPIO 17)
-ARDUINO = serial.Serial('/dev/ttyACM0', 9600, timeout=.01)  # setup serial port, baud rate, and timeout
+ARDUINO = serial.Serial('/dev/ttyACM1', 9600, timeout=.01)  # setup serial port, baud rate, and timeout
 # Set the QtQuick Style
 # Acceptable values: Default, Fusion, Imagine, Material, Universal.
 os.environ['QT_QUICK_CONTROLS_STYLE'] = (sys.argv[1] if len(sys.argv) > 1 else "Default")
@@ -66,7 +66,7 @@ class FishCommandWindow(qtw.QMainWindow, Ui_MainWindow):
         # self.y_data = pd.Series()
         # self.x_data = pd.Series()
         # ani = animation.FuncAnimation(self.fig, self.start_plotting, interval=10, blit=True, save_count=self.num_rows)
-        # self.show()
+        self.show()
 
     def connect_buttons(self):
         """Connect signals from each button to their corresponding methods"""
